30a64cf92b419d6f9d231d4688722f44be1d3df5
thorrfinnn <145729717+thorrfinnn@users.noreply.github.com>
2023-11-18 13:42:38 +0100

    Update 2.php

--- 2.php
+++ 2.php
@@ -1,6 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
-
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Audio Player</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <style>
+        /* Center the audio player */
+        .center-audio {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh; 
+        }
+        
+    </style>
+</head>
 <body>
 
 <?php 
